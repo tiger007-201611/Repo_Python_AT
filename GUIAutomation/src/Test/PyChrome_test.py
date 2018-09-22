9c42a8c21d3753ec20b48ed46869e574e60925dd
Tiger007William <Tiger007William@sohu.com>
2018-09-22 12:40:07 +0800

    check for webdriver

---
+++ GUIAutomation/src/Test/PyChrome_test.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 22, 2018
+
+@author: admin
+'''
+import selenium
+import time
+from selenium import webdriver
+
+def test_CHBrowser():
+    browser = webdriver.Chrome('/Users/admin/Automation/GitHub/Repo_Python_AT/GUIAutomation/webdriver/Chrome/chromedriver_mac64_v2.41_CH67-69')
+    time.sleep(10)
+    browser.get('http://www.baidu.com')
+    time.sleep(10)
+    browser.get('http://www.sohu.com')
+    time.sleep(10)
+    browser.close()
+    
+test_CHBrowser()
---
+++ GUIAutomation/src/Test/PyFirefox_test.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 22, 2018
+
+@author: admin
+'''
+import selenium
+import time
+from selenium import webdriver
+
+def test_FFBrowser():
+    browser = webdriver.Firefox()
+    time.sleep(10)
+    browser.get('http://www.baidu.com')
+    time.sleep(10)
+    browser.get('http://www.sohu.com')
+    time.sleep(10)
+    browser.get('http://192.168.1.11:8686/view/PythonAT/job/MyFirstPyProject/ws/tiger/log.html')
+    time.sleep(10)
+    browser.close()
+test_FFBrowser()
